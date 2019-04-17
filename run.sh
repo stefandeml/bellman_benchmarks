59166289fd5da49d9b426275b706c3ffcba0e945
Stefan <stefandeml@gmail.com>
2019-04-17 18:56:07 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 for i in 3 5 10
 do
   echo "Start run with $i bytes input"
-  cargo run --release  --bin groth16 384 $i &> cargo_log_$i.txt &
+  cargo run --release  --bin sonic 384 $i &> cargo_log_$i.txt &
   sleep 5
   pid=$(ps auxh | awk -v max=0 '{if($3>max){CPU=$3; PID=$2; NAME=$11; max=$3}} END{printf "%6d",PID}')
   python ps_mem.py -p $pid -w 10 > mem_log_$i.txt
